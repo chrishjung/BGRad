0453f8592d441272a565a23af5f05c4d4cd13777
chrishjung <44036117+chrishjung@users.noreply.github.com>
2022-02-28 09:51:39 -0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,36 @@
+function displayRadiationText()
+{
+    document.getElementById('RadiationText').style=" transition: opacity 2s; opacity:1"; 
+    animationDuration = "1s";
+}
+
+function hideRadiationText()
+{
+    document.getElementById('RadiationText').style="transition: opacity 0.5s; opacity:0";
+
+}
+
+function displayDeepSpaceText()
+{
+    document.getElementById('DeepSpaceText').style=" transition: opacity 2s; opacity:1"; 
+    animationDuration = "1s";
+}
+
+function hideDeepSpaceText()
+{
+    document.getElementById('DeepSpaceText').style="transition: opacity 0.5s; opacity:0";
+
+}
+
+function displayLowEarthText()
+{
+    document.getElementById('LowEarthText').style=" transition: opacity 2s; opacity:1"; 
+    animationDuration = "1s";
+}
+
+function hideLowEarthText()
+{
+    document.getElementById('LowEarthText').style="transition: opacity 0.5s; opacity:0";
+
+}
+
