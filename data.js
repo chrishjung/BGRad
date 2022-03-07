cb1de80b4b530ceee13111332d020d2104ab8f68
chrishjung <44036117+chrishjung@users.noreply.github.com>
2022-03-06 23:58:18 -0800

    Add files via upload

--- data.js
+++ data.js
@@ -34,3 +34,39 @@ function hideLowEarthText()
 
 }
 
+function AirplaneText()
+{
+    document.getElementById('AirplaneText').style=" transition: opacity 2s; opacity:1"; 
+    animationDuration = "1s";
+}
+
+function hideAirplaneText()
+{
+    document.getElementById('AirplaneText').style="transition: opacity 0.5s; opacity:0";
+
+}
+function GeoText()
+{
+    document.getElementById('GeoText').style=" transition: opacity 2s; opacity:1"; 
+    animationDuration = "1s";
+}
+
+function hideGeoText()
+{
+    document.getElementById('GeoText').style="transition: opacity 0.5s; opacity:0";
+
+}
+function GroundLevelText()
+{
+    document.getElementById('GroundLevel').style=" transition: opacity 2s; opacity:1"; 
+    animationDuration = "1s";
+}
+
+function hideGroundLevelText()
+{
+    document.getElementById('GroundLevel').style="transition: opacity 0.5s; opacity:0";
+
+}
+
+
+
