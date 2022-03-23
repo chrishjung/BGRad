a4f616d62d7a30b50aceabda98122306bc7c36e5
chrishjung <44036117+chrishjung@users.noreply.github.com>
2022-03-22 22:14:31 -0700

    Add files via upload

--- aosElements.js
+++ aosElements.js
@@ -60,8 +60,7 @@ const sidetext = document.querySelector('.sidetext')
 window.addEventListener('scroll', () =>{
     const scrolled = window.pageYOffset;
     const val = scrolled * 0.5;
-    sidetext.style.transform = `translateY(${-2 *val}%) rotate(270deg)`;
-
+    sidetext.style.transform = `translateY(${-2 *val}%) rotate(270deg)`
 }
 
 )
