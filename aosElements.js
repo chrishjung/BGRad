4f4f8234da783a8f3fc12111a078b61f93eff954
chrishjung <44036117+chrishjung@users.noreply.github.com>
2022-04-12 11:24:36 -0700

    Add files via upload

--- aosElements.js
+++ aosElements.js
@@ -125,9 +125,4 @@ function throttle(fn, delay){
     }
 }
 
-var scrollEventHandler = function()
-{
-  window.scroll(0, window.pageYOffset)
-}
 
-window.addEventListener("scroll", scrollEventHandler, false);
